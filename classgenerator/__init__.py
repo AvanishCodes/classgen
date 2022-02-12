f5458788b3883e8fab5708562b1d5f1ca609f154
Avanish Gupta <avanishcodes@gmail.com>
2022-02-12 16:14:42 +0530

    Separate the command line file

--- classgenerator/__init__.py
+++ classgenerator/__init__.py
@@ -1,31 +1,6 @@
 from sys import modules
 from .classgenerator import ClassGenerator
-from argparse import ArgumentParser
 
-# Command line arguments
-parser = ArgumentParser(description='Generate a class file.')
-parser.add_argument(
-    'className', 
-    type=str, 
-    help='The name of the class.'
-)
-parser.add_argument(
-    'members', 
-    type=str, 
-    help='The members of the class.'
-)
-args = parser.parse_args()
-if type(args.className) == str:
-    cg = ClassGenerator(
-        args.className,
-        args.members.split(','),
-        args.members.split(',')
-    )
-    cg.generate()
-    print(f'Class {args.className} generated successfully.')
-else:
-    print('Error: Please follow the convention of the command line arguments.')
-    print('Example: python3 ClassGenerator Student marks,grade,address')
 
 # export the ClassGenerator class
 modules['__all__'] = ['ClassGenerator']
--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as file:
 
 setup(
     name="classgen",                            # This is the name of the package
-    version="0.0.5",                            # The initial release version
+    version="0.0.6",                            # The initial release version
     author="Avanish Gupta",                     # Full name of the author
     description="Generate classes in Python using command line arguments",
     long_description=long_description,      # Long description read from the the readme file
@@ -16,9 +16,9 @@ setup(
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
-    ],                                      # Information to filter the project on PyPi website
-    python_requires='>=3.6',                # Minimum version requirement of the package
-    py_modules=["quicksample"],             # Name of the python package
+    ],                                       # Information to filter the project on PyPi website
+    python_requires='>=3.6',                 # Minimum version requirement of the package
+    py_modules=["classgen"],                 # Name of the python package
     install_requires=[],                     # Install other dependencies if any
     keywords=['Class', 'generate', 'automation', 'command line arguments', 'cli'],
     # package_dir={'':'classgenerator'},     # Directory of the source code of the package
